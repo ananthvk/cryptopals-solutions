ddc9135cf6ebe94ac680c8a055ba21f429950151
ananthvk <123456789987654321sr@gmail.com>
2024-06-10 02:55:09 +0530

    completed challenge 4

---
+++ tests/set1/challenge4.cpp
@@ -0,0 +1,128 @@
+#include "crypto.hpp"
+#include <fstream>
+#include <iostream>
+
+// Contains the frequency distribution for letters in english,
+// characters with lower index are more frequent
+static std::string frequency_dist = " etaoinshrdlcumwfgypbvkjxqz";
+
+static int score_table[256] = {0};
+
+// This function must be called before using score_table
+void build_score_table()
+{
+    for (int i = 0; i < 256; i++)
+    {
+        if (isalnum(i) || isspace(i))
+            score_table[i] = 1;
+        else
+            score_table[i] = -1;
+    }
+    int i = static_cast<int>(frequency_dist.size());
+    for (const auto &ch : frequency_dist)
+    {
+        score_table[static_cast<byte>(ch)] = i;
+        score_table[static_cast<byte>(toupper(ch))] = i;
+        --i;
+    }
+}
+
+// Calculates a score for a sequence of bytes
+// Higher the score, higher the probability that it is a piece of english text
+// If a non alphanumeric character is found, the score is reduced
+
+bool is_special_or_digit(byte ch)
+{
+    // Assuming ASCII
+    return ('0' <= ch && ch <= '9') || (33 <= ch && ch <= 64) || (91 <= ch && ch <= 96) ||
+           (123 <= ch && ch <= 126);
+}
+
+int calculate_score(bytes &text)
+{
+    int final_score = 0;
+    int num_special = 0;
+    for (const auto &ch : text)
+    {
+        final_score += score_table[ch];
+        num_special += is_special_or_digit(ch);
+    }
+    // To remove all the minus ones
+    final_score += num_special;
+
+    return final_score;
+}
+
+bytes single_byte_XOR(const bytes &plaintext, byte key)
+{
+    bytes ciphertext;
+    ciphertext.reserve(plaintext.size());
+    for (const auto &b : plaintext)
+    {
+        ciphertext.push_back(b ^ key);
+    }
+    return ciphertext;
+}
+
+std::string decode(const std::string &hexstring, int &key, int &calculated_score)
+{
+    auto byts = hex::to_bytes(hexstring);
+    int max_score = 0;
+    bytes english_plaintext;
+
+    // Brute force, check all the bytes and print the one with the highest english score
+    for (int i = 0; i < 256; i++)
+    {
+        auto plaintext = single_byte_XOR(byts, static_cast<byte>(i));
+        int score = calculate_score(plaintext);
+        if (score > max_score)
+        {
+            max_score = score;
+            english_plaintext = plaintext;
+            key = i;
+        }
+    }
+
+    std::string plaintext;
+    std::copy(english_plaintext.begin(), english_plaintext.end(), std::back_inserter(plaintext));
+    calculated_score = max_score;
+    return plaintext;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./challenge4 <filename>" << std::endl;
+        return 1;
+    }
+    std::ifstream ifs(argv[1]);
+    if (!ifs)
+    {
+        std::cout << "Could not open " << argv[1] << std::endl;
+    }
+    build_score_table();
+    std::string line;
+
+    int max_score = 0;
+    int max_score_key = 0;
+    std::string possible_plaintext;
+    std::string possible_ciphertext;
+
+    while (std::getline(ifs, line))
+    {
+        int key = 0, score = 0;
+        auto decoded = decode(line, key, score);
+        if (score > max_score)
+        {
+            max_score = score;
+            max_score_key = key;
+            possible_plaintext = decoded;
+            possible_ciphertext = line;
+        }
+    }
+    std::cout << "Among the given lines, " << std::endl;
+    std::cout << possible_ciphertext << std::endl;
+    std::cout << "Is likely to be XOR encrypted with key " << max_score_key << " and has a score " << max_score << std::endl;
+    std::cout << "Plaintext: " << possible_plaintext << std::endl;
+}
